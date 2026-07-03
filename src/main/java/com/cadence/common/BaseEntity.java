cf610aba0385db9ce6baab604b85cd037b596477
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2026-07-03 18:30:10 +0530

    2. user entity, repository and REST endpoints creation

---
+++ src/main/java/com/cadence/common/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.cadence.common;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
---
+++ src/main/java/com/cadence/user/User.java
@@ -0,0 +1,25 @@
+package com.cadence.user;
+
+import com.cadence.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+}
